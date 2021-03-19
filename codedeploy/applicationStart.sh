05d3c7538161df5e3d7891cde1e4ce2a4ed53c2a
phu tran <tran.ph@northeastern.edu>
2021-03-18 17:10:03 -0700

    sth new

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,4 +2,5 @@
 
 export user
 sudo /var/lib/cloud/instance/scripts/part-001
-nohup python3 /home/ubuntu/app/application/main.py
+nohup python3 /home/ubuntu/app/application/main.py > /dev/null 2> /dev/null < /dev/null &
+
