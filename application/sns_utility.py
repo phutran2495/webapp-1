da3692c4e55a4bc6d405f1ee26657b505b3f62e4
phu tran <tran.ph@northeastern.edu>
2021-04-06 12:54:47 -0700

    add sns

---
+++ application/sns_utility.py
@@ -0,0 +1,19 @@
+import json
+
+from botocore.exceptions import ClientError
+
+class SNSWrapper:
+    def __init__(self, sns_resource):
+        self.sns_resource = sns_resource
+        
+
+    def publish_message(self, topic_arn, message):
+        try:
+            response = self.sns_resource.publish(TopicArn=topic_arn, Message=json.dumps(message))
+            message_id = response['MessageId']
+            
+        except ClientError:
+            print("error occurred when sending message")
+            
+        else:
+            return message_id
